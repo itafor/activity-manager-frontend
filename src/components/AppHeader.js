362a5f5359cf82834b5fc9fa990fe803a3684d8a
“4larin” <folarin.tunji98@gmail.com>
2022-09-18 17:39:00 +0100

    fix: autoLayout

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,27 +1,61 @@
-import { Button } from "antd";
-import React from "react";
-import { useDispatch } from "react-redux";
-import { logout } from "../redux/authSlice";
+import { Button } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleCollapseSider, toggleSiderHidden } from '../redux/appSlice'
+import { logout } from '../redux/authSlice'
 
 const AppHeader = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
+  const [deviceWidth, setDeviceWidth] = useState(window.innerWidth)
+  const { siderHidden, siderCollapsed } = useSelector((state) => state.app)
+
+  useEffect(() => {
+    function handleResize() {
+      setDeviceWidth(window.innerWidth)
+    }
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  useEffect(
+    () => {
+      // if (deviceWidth <= 640 && siderHidden === false) {
+      //   dispatch(toggleSiderHidden())
+      // }
+      if (deviceWidth < 1024 && deviceWidth > 640 && siderHidden) {
+        dispatch(toggleSiderHidden(false))
+        dispatch(toggleCollapseSider(true))
+      }
+      if (deviceWidth < 1024 && !siderCollapsed) {
+        dispatch(toggleCollapseSider(true))
+      }
+      if (deviceWidth >= 1024 && siderCollapsed === true) {
+        dispatch(toggleCollapseSider(false))
+      }
+    },
+    // eslint-disable-next-line
+    [deviceWidth],
+  )
 
   return (
     <header
-      className="site-layout-background"
+      className='site-layout-background'
       style={{
-        display: "flex",
+        display: 'flex',
         padding: 0,
-        alignItems: "center",
-        justifyContent: "space-between",
+        alignItems: 'center',
+        justifyContent: 'space-between',
       }}
     >
       <h1>App Header </h1>
-      <Button type="primary" onClick={() => dispatch(logout())}>
+      <Button type='primary' onClick={() => dispatch(logout())}>
         Logout
       </Button>
     </header>
-  );
-};
+  )
+}
 
-export default AppHeader;
+export default AppHeader
