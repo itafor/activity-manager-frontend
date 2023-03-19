9697dbdae11c4e3a5625e803d38474b140e937b4
itafor <itaforfrancis@gmail.com>
2023-03-19 09:39:34 +0100

    dashboard updated

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getAllUsers } from '../../redux/userSlice'
 import { PageHeader } from 'antd'
 import UserTable from '../Users/UserTable'
+import styled from 'styled-components'
 
 const Dashboard = () => {
   const { users } = useSelector((state) => state)
@@ -36,77 +37,78 @@ const Dashboard = () => {
   return (
     <div>
       <PageHeader extra={[]} title='Dashboard' />
-
-      <CardGroup>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <Link to='insurances'>
-                <Link to='/insurances'>Insurances</Link>
-              </Link>
-            </Card.Title>
-            <Card.Text>{dashboardStats?.insurances_count}</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <Link to='/categories'>Categories</Link>
-            </Card.Title>
-            <Card.Text>{dashboardStats?.category_count}</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <Link to='/companies'>Companies</Link>
-            </Card.Title>
-            <Card.Text>{dashboardStats?.companies_count}</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <Link to='/orders'>Orders</Link>
-            </Card.Title>
-            <Card.Text>{dashboardStats?.orders_count}</Card.Text>
-          </Card.Body>
-        </Card>
-      </CardGroup>
-      <CardGroup>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <Link to='/payments'>Payments</Link>
-            </Card.Title>
-            <Card.Text>{dashboardStats?.payments_count}</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <Link to='/users'>Users</Link>
-            </Card.Title>
-            <Card.Text>{dashboardStats?.users_count}</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <Link to='/claims'>Claims</Link>
-            </Card.Title>
-            <Card.Text>{dashboardStats?.claims_count}</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <Link to='/lessons'>Lessons</Link>
-            </Card.Title>
-            <Card.Text>{dashboardStats?.lessons_count}</Card.Text>
-          </Card.Body>
-        </Card>
-      </CardGroup>
+      <StyledContainer>
+        <CardGroup className='card-group'>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <Link to='insurances'>
+                  <Link to='/insurances'>Insurances</Link>
+                </Link>
+              </Card.Title>
+              <Card.Text>{dashboardStats?.insurances_count}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <Link to='/categories'>Categories</Link>
+              </Card.Title>
+              <Card.Text>{dashboardStats?.category_count}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <Link to='/companies'>Companies</Link>
+              </Card.Title>
+              <Card.Text>{dashboardStats?.companies_count}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <Link to='/orders'>Orders</Link>
+              </Card.Title>
+              <Card.Text>{dashboardStats?.orders_count}</Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+        <CardGroup className='card-group'>
+          <Card>
+            <Card.Body>
+              <Card.Title className='card-title'>
+                <Link to='/payments'>Payments</Link>
+              </Card.Title>
+              <Card.Text>{dashboardStats?.payments_count}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <Link to='/users'>Users</Link>
+              </Card.Title>
+              <Card.Text>{dashboardStats?.users_count}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <Link to='/claims'>Claims</Link>
+              </Card.Title>
+              <Card.Text>{dashboardStats?.claims_count}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <Link to='/lessons'>Lessons</Link>
+              </Card.Title>
+              <Card.Text>{dashboardStats?.lessons_count}</Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </StyledContainer>
       <PageHeader extra={[]} title='Users' />
       <UserTable data={users.data} loading={users.loading} />
     </div>
@@ -114,3 +116,16 @@ const Dashboard = () => {
 }
 
 export default Dashboard
+const StyledContainer = styled.div`
+  .ant-card-meta {
+    align-items: center !important;
+  }
+  .card-group {
+    font-size: 14px;
+    font-family: Arial;
+  }
+  .card-title {
+    font-size: 14px;
+    font-family: Arial;
+  }
+`
